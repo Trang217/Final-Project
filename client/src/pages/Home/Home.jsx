5e1732cff9679675b5e4023a8c252de7bb31d20d
Aleksandra Bury <aleksandra.w.bury@gmail.com>
2022-01-21 10:16:59 +0100

    Home Page file structure

---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,25 @@
+// ---- hooks, dependencies, styling import ----
+
+// ---- components ----
+
+// ---- context import ----
+
+// ---- data ----
+
+// ---- COMPONENT ----
+const Home = () => {
+  //? ---- hooks ----
+
+  //? ---- event handlers ----
+
+  //? ---- variables ----
+
+  //? ---- rendering ----
+  return <>
+<div className="">
+    {/*adds the map background */}
+</div>
+  </>;
+};
+
+export default Home;
