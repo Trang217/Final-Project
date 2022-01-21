552bce26d879f955885a7bf5efe051cea7afe940
Aleksandra Bury <aleksandra.w.bury@gmail.com>
2022-01-21 09:58:58 +0100

    landing page skeleton w/comments

--- client/src/pages/Landing/Landing.jsx
+++ client/src/pages/Landing/Landing.jsx
@@ -1,7 +1,44 @@
-import React from 'react';
+// ---- hooks, dependencies, styling import ----
 
+// ---- components ----
+import AnimalBubble from "./AnimalBubble";
+
+// ---- COMPONENT ----
 const Landing = () => {
-  return <div><h3>Landing</h3></div>;
+  //? ---- hooks ----
+  // const history = useHistory();
+
+  //? ---- event handlers ----
+
+  //Start the adventure button handler
+  const handleSubmit = () => {};
+
+  /* QUESTION: useHistory here? {history.push('/login')*/
+
+  //? ---- rendering ----
+  return (
+    <>
+      <div className="">
+        {/* applies the map picture */}
+        <div className="">
+          <p>
+            Hey there, little explorer! create an account or sign in to
+            playfully discover some interesting facts about our planet’s
+            astonishing landscapes, amazing wildlife, and diverse flora.
+          </p>
+          <p>
+            At the end of each exploration you can do a fun little quiz to test
+            your knowledge and collect some badges on your profile!
+          </p>
+          <button onClick={handleSubmit}>Start the adventure</button>
+          {/* NAVIGATE - LINK type*/}
+        </div>
+        <AnimalBubble name="item_01" />
+        <AnimalBubble name="item_02" />
+        <AnimalBubble name="item_03" />
+      </div>
+    </>
+  );
 };
 
 export default Landing;
