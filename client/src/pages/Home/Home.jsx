2e7182fb1506414cbc7d2f32804e078c841c821b
Aleksandra Bury <aleksandra.w.bury@gmail.com>
2022-01-21 13:18:03 +0100

    Home skeleton with react-modal temporarily

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,29 +1,51 @@
 // ---- hooks, dependencies, styling import ----
-
+import { useState } from "react";
+import Modal from "react-modal"; //! I installed a dependency locally - can be replaced by Tailwind Modal
+import "./dummy-style.scss";
 // ---- components ----
 
 import AnimalBubble from "../../components/AnimalBubble";
-
-// ---- context import ----
-
-// ---- data ----
+import Story from "./Story";
 
 // ---- COMPONENT ----
 
 const Home = () => {
   //? ---- hooks ----
 
-  //? ---- event handlers ----
-  //onClick={() => history.push(`/${basePath}/${id}`)}
+  const [isOpen, setIsOpen] = useState(false);
 
-  //? ---- variables ----
+  //? ---- event handlers ----
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  const navigateToProfile = () => {
+    console.log("Player clicked, navigate to profile");
+  };
 
   //? ---- rendering ----
   return (
     <div>
-      <AnimalBubble name="rainforest" doesNavigate="true"/>
-      <AnimalBubble name="desert" doesNavigate="true"/>
-      <AnimalBubble name="ocean" doesNavigate="true"/>
+      <div>
+        <div onClick={navigateToProfile}>Player Animated</div>
+        <div onClick={openModal}>Read the journal trigger</div>
+        <Modal isOpen={isOpen} ariaHideApp={false}>
+          <Story />
+          <div onClick={closeModal}>Explore</div>
+        </Modal>
+      </div>
+
+      <div>
+        <AnimalBubble name="rainforest" doesNavigate="true" />{" "}
+        {/*triggers onClick function within the component */}
+        <AnimalBubble name="desert" doesNavigate="true" />
+        <AnimalBubble name="ocean" doesNavigate="true" />
+      </div>
     </div>
   );
 };
