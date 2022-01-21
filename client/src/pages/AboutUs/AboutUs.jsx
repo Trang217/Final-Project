b50bdd77713900f646e8792c2287df03e79790af
Aleksandra Bury <aleksandra.w.bury@gmail.com>
2022-01-21 14:49:33 +0100

    Pages structured

---
+++ client/src/pages/AboutUs/AboutUs.jsx
@@ -0,0 +1,21 @@
+// ---- hooks, dependencies, styling import ----
+
+// ---- components ----
+
+// ---- context import ----
+
+// ---- data ----
+
+// ---- COMPONENT ----
+
+const AboutUs = () => {
+  //? ---- hooks ----
+
+  //? ---- event handlers ----
+
+  //? ---- variables ----
+
+  //? ---- rendering ----
+  return <div>About Us</div>;
+};
+export default AboutUs ;
---
+++ client/src/pages/MyBadges/MyBadges.jsx
@@ -0,0 +1,21 @@
+// ---- hooks, dependencies, styling import ----
+
+// ---- components ----
+
+// ---- context import ----
+
+// ---- data ----
+
+// ---- COMPONENT ----
+
+const MyBadges = () => {
+  //? ---- hooks ----
+
+  //? ---- event handlers ----
+
+  //? ---- variables ----
+
+  //? ---- rendering ----
+  return <div>Badges</div>;
+};
+export default MyBadges ;
