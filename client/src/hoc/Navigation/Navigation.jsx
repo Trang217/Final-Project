f11ef89ce414e41b2012014853294e2300f79607
Luisa-Lisanne Forck <luisalisanne.forck@posteo.de>
2022-01-22 15:18:53 +0100

    Add navlinks for testing

--- client/src/hoc/Navigation/Navigation.jsx
+++ client/src/hoc/Navigation/Navigation.jsx
@@ -1,5 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Navigation() {
-  return <div></div>;
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/home">Home</Link>
+        </li>
+        <li>
+          <Link to="/badges">My Badges</Link>
+        </li>
+        <li>
+          <Link to="/about">About Us</Link>
+        </li>
+        <li>
+          <Link to="/account">Account Details</Link>
+        </li>
+        <li>Logout</li>
+      </ul>
+    </nav>
+  );
 }
