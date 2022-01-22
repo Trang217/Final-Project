b1110b22703c23436dd9bc4d8b1209d897b4b266
Luisa-Lisanne Forck <luisalisanne.forck@posteo.de>
2022-01-22 15:19:51 +0100

    Import MainRouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import MainRouter from "./hoc/MainRouter/MainRouter";
 
 const App = () => {
   return (
     <div className="app">
       <h1>Final Project!</h1>
+      <MainRouter />
     </div>
   );
 };
