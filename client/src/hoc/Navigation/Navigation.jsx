cfc1c440db677c127f8d199432d57784e257089b
Luisa-Lisanne Forck <luisalisanne.forck@posteo.de>
2022-01-26 13:38:19 +0100

    Add padding to Links

--- client/src/hoc/Navigation/Navigation.jsx
+++ client/src/hoc/Navigation/Navigation.jsx
@@ -5,28 +5,29 @@ import closeSidebar from "./Sidebar";
 export default function Navigation() {
   return (
     <nav>
-      <ul>
-        <li>
+      <ul className="p-2">
+        <li className="p-4">
           <Link to="/home" onClick={closeSidebar}>
             Home
           </Link>
         </li>
-        <li>
+        <li className="p-4">
           <Link to="/badges" onClick={closeSidebar}>
             My Badges
           </Link>
         </li>
-        <li>
+        <li className="p-4">
           <Link to="/about" onClick={closeSidebar}>
             About Us
           </Link>
         </li>
-        <li>
+        <hr></hr>
+        <li className="p-4">
           <Link to="/account" onClick={closeSidebar}>
             Account Details
           </Link>
         </li>
-        <li>Logout</li>
+        <li className="p-4">Logout</li>
       </ul>
     </nav>
   );
