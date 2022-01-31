98ce7d9c80b9c7f7fe604a6e4a1fe39c3fd89643
Luisa-Lisanne Forck <luisalisanne.forck@posteo.de>
2022-01-31 10:58:58 +0100

    add route parameters

--- routes/contentRoutes.js
+++ routes/contentRoutes.js
@@ -3,7 +3,7 @@ const controller = require("../controllers/contentController");
 
 const router = express.Router();
 
-router.get("/game", controller.getGameContent);
-router.get("/quiz", controller.getQuizContent);
+router.get("/game/:biome", controller.getGameContent);
+router.get("/quiz/:biome", controller.getQuizContent);
 
 module.exports = router;
