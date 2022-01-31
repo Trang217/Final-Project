f9165d4578e1784e799528a21595bf3e23acf275
Luisa-Lisanne Forck <luisalisanne.forck@posteo.de>
2022-01-31 10:55:48 +0100

    Import AppError

--- controllers/contentController.js
+++ controllers/contentController.js
@@ -1,32 +1,30 @@
 const Content = require("../models/Content");
+const AppError = require("../error/AppError");
 const { tryCatchHelper } = require("../helpers/tryCatchHelper");
 
 //* get game content (items)
 
 exports.getGameContent = tryCatchHelper(async (req, res, next) => {
-  const search = req.body.search;
-  const items = await Content.findOne({ ecosystem: search }).select(
-    "ecosystem items"
-  );
+  const search = { ecosystem: req.params.biome };
+  const data = await Content.findOne(search).select("ecosystem items");
+  console.log(data);
 
-  if (!items) {
+  if (!data) {
     return next(new AppError("No items here!", 404));
   }
 
   return res
     .status(200)
-    .json({ status: "success", message: "Found items", items });
+    .json({ status: "success", message: "Found data", data });
 });
 
 //* get quiz content (questions)
 
 exports.getQuizContent = tryCatchHelper(async (req, res, next) => {
-  const search = req.body.search;
-  const questions = await Content.findOne({ ecosystem: search }).select(
-    "ecosystem questions"
-  );
+  const search = { ecosystem: req.params.biome };
+  const questions = await Content.findOne(search).select("ecosystem questions");
 
-  if (!questions) {
+  if (!search) {
     return next(new AppError("No questions here!", 404));
   }
 
