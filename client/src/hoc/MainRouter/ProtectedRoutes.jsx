72613a74dbfbdcb0149337aaf07f6fb69945bf5b
Luisa-Lisanne Forck <luisalisanne.forck@posteo.de>
2022-02-01 13:18:44 +0100

    useContext to check if user is logged in and authenticated

--- client/src/hoc/MainRouter/ProtectedRoutes.jsx
+++ client/src/hoc/MainRouter/ProtectedRoutes.jsx
@@ -1,16 +1,12 @@
-import React from "react";
+import { useContext } from "react";
 import { Outlet } from "react-router-dom";
+
+import { AuthContext } from "../../contexts/AuthContext/AuthContext";
 import LoginAndRegistration from "../../pages/LoginAndRegistration/LoginAndRegistration";
 
-//! This would use AuthContext to check if user is properly authenticated
-const useAuth = () => {
-  const user = { loggedIn: true };
-  return user && user.loggedIn;
-};
-
 const ProtectedRoutes = () => {
-  const isAuth = useAuth();
-  return isAuth ? <Outlet /> : <LoginAndRegistration />;
+  const { loggedIn } = useContext(AuthContext);
+  return loggedIn ? <Outlet /> : <LoginAndRegistration />;
 };
 
 export default ProtectedRoutes;
