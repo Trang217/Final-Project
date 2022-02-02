13a22f6b23fe2929fafbc7c02f70c40892f038df
Luisa-Lisanne Forck <luisalisanne.forck@posteo.de>
2022-02-02 10:16:00 +0100

    closes #60, cleanup code

--- client/src/pages/Landing/Landing.jsx
+++ client/src/pages/Landing/Landing.jsx
@@ -16,56 +16,50 @@ const Landing = () => {
   //? ---- event handlers ----
 
   //Start the adventure button handler
-  const handleSubmit = () => {
+  const handleClick = () => {
     navigate("/login");
   };
 
-  //? ---- event handlers ----
-
-  const showAboutUs = () => {
+  // Show Modal with AboutUS component
+  const handleModal = () => {
     setModalOpen(!modalOpen);
   };
 
-  /* QUESTION: useHistory here? {history.push('/login')*/
-
   //? ---- rendering ----
   return (
-    <>
+    <div className="">
+      {/* applies the map picture */}
       <div className="">
-        {/* applies the map picture */}
-        <div className="">
-          <button
-            className="fixed z-30 cursor-pointer right-10 top-6 p-3 w-20 bg-red-500"
-            onClick={showAboutUs}
-          >
-            about the project
-          </button>
-          <p>
-            Hey there, little explorer! create an account or sign in to
-            playfully discover some interesting facts about our planet’s
-            astonishing landscapes, amazing wildlife, and diverse flora.
-          </p>
-          <p>
-            At the end of each exploration you can do a fun little quiz to test
-            your knowledge and collect some badges on your profile!
-          </p>
-          <button className="bg-yellow-500 p-3 m-2" onClick={handleSubmit}>
-            Start the adventure
-          </button>
-          {/* NAVIGATE - LINK type*/}
-        </div>
-        <AnimalBubble name="rainforest" />
-        <AnimalBubble name="desert" />
-        <AnimalBubble name="ocean" />
-
-        {modalOpen ? (
-          <Modal isOpen={modalOpen}>
-            {" "}
-            <AboutUs />
-          </Modal>
-        ) : null}
+        <button
+          className="fixed z-30 cursor-pointer right-10 top-6 p-3 w-20 bg-red-500"
+          onClick={handleModal}
+        >
+          about the project
+        </button>
+        <p>
+          Hey there, little explorer! create an account or sign in to playfully
+          discover some interesting facts about our planet’s astonishing
+          landscapes, amazing wildlife, and diverse flora.
+        </p>
+        <p>
+          At the end of each exploration you can do a fun little quiz to test
+          your knowledge and collect some badges on your profile!
+        </p>
+        <button className="bg-yellow-500 p-3 m-2" onClick={handleClick}>
+          Start the adventure
+        </button>
       </div>
-    </>
+      <AnimalBubble name="rainforest" />
+      <AnimalBubble name="desert" />
+      <AnimalBubble name="ocean" />
+
+      {modalOpen ? (
+        <Modal isOpen={modalOpen}>
+          {" "}
+          <AboutUs />
+        </Modal>
+      ) : null}
+    </div>
   );
 };
 
