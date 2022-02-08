bbe01f3b6adfc0c3811cf954dbedbec49d85a09e
Luisa-Lisanne Forck <luisalisanne.forck@posteo.de>
2022-02-08 12:01:30 +0100

    Add test style to Badges page and badge element

---
+++ client/src/pages/MyBadges/Badge.jsx
@@ -0,0 +1,35 @@
+// ---- hooks, dependencies, styling import ----
+import { format } from "date-fns";
+import { useNavigate } from "react-router-dom";
+import capitalize from "../../utils/capitalize";
+
+// ---- COMPONENT ----
+
+const Badge = ({ badge }) => {
+  //? ---- hooks ----
+  const navigate = useNavigate();
+
+  //? ---- event handlers ----
+
+  const toQuiz = () => {
+    navigate(`/quiz/${type}`);
+  };
+  //? ---- variables ----
+
+  const { type, score, date } = badge;
+
+  //? ---- rendering ----
+  return (
+    <div className="oneBadge">
+      <p className="type">{capitalize(type)}</p>
+      <p className="score">score: {score}</p>
+      {score > 0 ? (
+        <p className="date">
+          discovered on: {format(new Date(date), "EEEE, do MMMM yyyy")}
+        </p>
+      ) : null}
+      {score > 0 ? <button onClick={toQuiz}>Do the quiz again</button> : null}
+    </div>
+  );
+};
+export default Badge;
