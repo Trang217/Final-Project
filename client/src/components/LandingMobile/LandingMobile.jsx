a123f0b15b7c322f30ec9e0205852868c1d4b2b3
Luisa-Lisanne Forck <luisalisanne.forck@posteo.de>
2022-02-10 16:05:02 +0100

    Mobile Landing content and style

--- client/src/components/LandingMobile/LandingMobile.jsx
+++ client/src/components/LandingMobile/LandingMobile.jsx
@@ -1,5 +1,5 @@
 // ---- hooks, dependencies, styling import ----
-import { useNavigate } from "react-router-dom";
+import AnimalBubble from "../AnimalBubble";
 
 // ---- components ----
 
@@ -12,7 +12,37 @@ const LandingMobile = () => {
   //? ---- event handlers ----
 
   //? ---- rendering ----
-  return <div>Mobile Landing</div>;
+  return (
+    <div className="background">
+      <div className="landingContent">
+        <AnimalBubble name="rainforest" type="mobile" />
+        <AnimalBubble name="desert" type="mobile" />
+        <AnimalBubble name="ocean" type="mobile" />
+        <p className="title">Hey there!</p>
+        <p>
+          When you open this website on a larger device you can create an
+          account or sign in to discover some interesting facts about our
+          planet’s astonishing landscapes, amazing wildlife, and diverse flora.
+          At the end of each exploration you'll be able to do a fun little quiz
+          to test your knowledge and collect some badges on your profile!
+        </p>
+        <p>
+          This website was created as a final project by four developers
+          graduating from Digital Career Institute in February 2022 who are
+          passionate about coding, art, animation, and our planet.
+        </p>
+        <button>
+          <a
+            href="https://github.com/Trang217/Final-Project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            See the repo on GitHub
+          </a>
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default LandingMobile;
