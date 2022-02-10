d6dc7cc27e231f6ac126667f8fd10ef4c70a6f70
Luisa-Lisanne Forck <luisalisanne.forck@posteo.de>
2022-02-10 14:31:13 +0100

    Add Mobile Landing page sekeleton jsx and scss, link to main.scss

---
+++ client/src/components/LandingMobile/LandingMobile.jsx
@@ -0,0 +1,18 @@
+// ---- hooks, dependencies, styling import ----
+import { useNavigate } from "react-router-dom";
+
+// ---- components ----
+
+// ---- context ----
+
+// ---- COMPONENT ----
+const LandingMobile = () => {
+  //? ---- hooks ----
+
+  //? ---- event handlers ----
+
+  //? ---- rendering ----
+  return <div>Mobile Landing</div>;
+};
+
+export default LandingMobile;
